b5b519401a7c8cbe88ddd7c26e4729f8dbd02416
101cade <118102920+101cade@users.noreply.github.com>
2023-03-02 10:49:41 -0600

    Add files via upload

---
+++ HelloMessages.cs
@@ -0,0 +1,24 @@
+namespace HelloMethods
+{
+    public class Message
+    {
+        public static string GetMessage(string lang)
+        {
+            if (lang.Equals("sp")) {
+                return "Hola Mundo";
+            }
+            else if (lang.Equals("fr"))
+            {
+                return "Bonjour le monde";
+            }
+            else if (lang.Equals("ge"))
+            {
+                return "Hallo Welt";
+            }
+            else
+            {
+                return "Hello World";
+            }        
+        }
+    }
+}
